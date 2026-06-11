742f0abf706d2d29aee17b24beb6ed084babe10c
mamac <ma5021135@gmail.com>
2026-06-11 17:39:28 +0200

    Remove env and build files

--- dist/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-const asyncHandler_1 = require("../middleware/asyncHandler");
-exports.authRouter = (0, express_1.Router)();
-exports.authRouter.post("/login", (0, asyncHandler_1.asyncHandler)(auth_controller_1.loginUser));
-exports.authRouter.post("/register", (0, asyncHandler_1.asyncHandler)(auth_controller_1.registerCustomer));
--- dist/src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const env_1 = require("./config/env");
-app_1.app.listen(env_1.env.port, () => {
-    console.log(`Steakz API running on http://localhost:${env_1.env.port}`);
-});
